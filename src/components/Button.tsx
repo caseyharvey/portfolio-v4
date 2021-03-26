f70c44d3325c74ecbc196176a5f17d9920216434
Casey Harvey <caseyharvey@caseys-air.home>
2021-03-26 23:02:11 +1100

    changed button css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Button: React.FC<Props> = ({ text }) => {
   return (
-    <button className="bg-gray-50 text-black w-full rounded p-3 text-xl">
+    <button className="bg-darkGray text-white border-2 border-gray-600 w-full rounded p-3 text-xl">
       {text}
     </button>
   )
